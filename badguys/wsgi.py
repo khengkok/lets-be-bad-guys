b8294895ff8df60bb154a58b7e9b975c0548de50
Kheng Kok Mar <khengkok@gmail.com>
2021-01-29 10:23:30 +0800

    Update wsgi.py

--- badguys/wsgi.py
+++ badguys/wsgi.py
@@ -22,6 +22,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badguys.settings")
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application2 = get_wsgi_application()
+
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
